3df4beed940a22c30fa17dbf6628671009a7be86
Brian Lalor <blalor@bravo5.org>
2014-01-30 11:46:48 -0500

    Working, tested Kickstart configs for VirtualBox and EC2
    Also locking root user, now.

---
+++ scripts/lockdown.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## lock the root account so it can't be logged into
+/usr/sbin/usermod -L root
+
+## -- alternatively? --
+## change the root password to a random string
+# tr -dc A-Za-z0-9 < /dev/urandom | head -c 32 | passwd --stdin root
