0d1b46afce89ba4e971d8a200d70e3bf8bec4c85
Brian Lalor <blalor@bluestatedigital.com>
2014-03-14 18:09:06 -0400

    Ensure ~ec2-user/.ssh exists

--- scripts/020-vagrant.sh
+++ scripts/020-vagrant.sh
@@ -8,8 +8,11 @@ set -e -u -x
 date > /etc/vagrant_box_build_time
 
 ## common baseline with ec2 instance
-/usr/sbin/useradd ec2-user
+[ -d ~ec2-user/.ssh ] || mkdir -p ~ec2-user/.ssh
 touch ~ec2-user/.ssh/authorized_keys
+chown -R ec2-user:ec2-user ~ec2-user/.ssh
+find ~ec2-user/.ssh -type f | xargs chmod 600
+find ~ec2-user/.ssh -type d | xargs chmod 700
 
 /usr/sbin/useradd vagrant
 echo vagrant | passwd --stdin vagrant
